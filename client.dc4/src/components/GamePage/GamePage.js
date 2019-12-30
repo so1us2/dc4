c4b96c9d6d4f97700ac897802860ec5e09624fef
Jake Mirra <jake@asteroidpm.com>
2019-12-30 15:36:10 -0600

    Game state is shown accurately

--- client.dc4/src/components/GamePage/GamePage.js
+++ client.dc4/src/components/GamePage/GamePage.js
@@ -8,17 +8,19 @@ import "styles/GamePage.css";
 export default class GamePage extends Component {
 
   render() {
-    let gameState = this.props.gameState
+    let gameState = this.props.gameState;
+    console.log("rendering GamePage with gameState:");
+    console.log(gameState);
     return (
       <div className="GamePage">
         <InformationPanel
-          gameState={gameState}
+          gameState={this.props.gameState}
           position={this.props.position}
           container={this.props.container}
           lastMove={this.props.lastMove}
         />
         <GamePanel
-          gameState={gameState}
+          gameState={this.props.gameState}
           position={this.props.position}
           myTurn={gameState.currentTurn === this.props.position}
           sendMove={this.props.container.sendMove}
--- client.dc4/src/components/GamePage/GamePanel.js
+++ client.dc4/src/components/GamePage/GamePanel.js
@@ -52,6 +52,8 @@ export default class GamePanel extends Component {
   }
 
   render() {
+    console.log("Rendering GamePanel with gameState:");
+    console.log(this.props.gameState);
     return (
       <div className="GamePanel">
         <svg
@@ -107,8 +109,40 @@ class BoardCell extends Component {
 }
 
 class Pieces extends Component {
+
+  expand = (moveHistory) => {
+    let ret = [];
+    moveHistory.forEach(move => {
+      ret.push({...move, row: this.columnCounts[move.column]++});
+    });
+    return ret;
+  }
+
   render() {
-    return null;
+    console.log("Rendering Pieces with gameState:");
+    console.log(this.props.gameState);
+    this.columnCounts = [0,0,0,0,0,0,0];
+    this.expandedMoves = this.expand(this.props.gameState.moveHistory);
+    let ret = [];
+    this.expandedMoves.forEach((move, i) => {
+      ret.push(
+        <GamePiece column={move.column} row={move.row} position={move.position} key={i} />
+      );
+    });
+    console.log(ret);
+    return ret;
+  }
+}
+
+class GamePiece extends Component {
+  getColor = () => {
+    return (this.props.position === "FIRST") ? "red" : "black";
+  }
+
+  render() {
+    return (
+      <circle cx={(this.props.column + 0.5).toString()} cy={(6 - this.props.row + 0.5).toString()} r="0.4" fill={this.getColor()}/>
+    );
   }
 }
 
